60f06f2ea2f81ebf113d41cb013d3eaa76245f48
robotescu <80849623+robotescu@users.noreply.github.com>
2024-04-07 19:19:42 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+    strip.show()
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+    strip.showRainbow(1, 360)
+    strip.show()
+    basic.pause(500)
+    strip.shift(1)
+})
